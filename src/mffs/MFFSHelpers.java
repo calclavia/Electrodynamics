ba6c70d91439d7f26b9cd6b06fa6acf7153f7a6e
Henry Mao <admin@calclavia.com>
2013-04-11 21:04:52 +0800

    Set up main class and function helper class

---
+++ src/mffs/MFFSHelpers.java
@@ -0,0 +1,56 @@
+package mffs;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * A class containing some general helpful functions.
+ * 
+ * @author Calclavia
+ * 
+ */
+public class MFFSHelpers
+{
+	public static List<String> splitStringPerWord(String string, int wordsPerLine)
+	{
+		String[] words = string.split(" ");
+		List<String> lines = new ArrayList<String>();
+
+		for (int lineCount = 0; lineCount < Math.ceil((float) words.length / (float) wordsPerLine); lineCount++)
+		{
+			String stringInLine = "";
+
+			for (int i = lineCount * wordsPerLine; i < Math.min(wordsPerLine + lineCount * wordsPerLine, words.length); i++)
+			{
+				stringInLine += words[i] + " ";
+			}
+
+			lines.add(stringInLine.trim());
+		}
+
+		return lines;
+	}
+
+	/**
+	 * Gets a compound from an itemStack.
+	 * 
+	 * @param itemStack
+	 * @return
+	 */
+	public static NBTTagCompound getNBTTagCompound(ItemStack itemStack)
+	{
+		if (itemStack != null)
+		{
+			if (itemStack.getTagCompound() == null)
+			{
+				itemStack.setTagCompound(new NBTTagCompound());
+			}
+
+			return itemStack.getTagCompound();
+		}
+
+		return null;
+	}
+}
---
+++ src/mffs/ModularForceFieldSystem.java
@@ -0,0 +1,39 @@
+package mffs;
+
+import java.util.logging.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = ModularForceFieldSystem.ID, name = ModularForceFieldSystem.NAME, version = ModularForceFieldSystem.VERSION, dependencies = "after:ThermalExpansion")
+@NetworkMod(clientSideRequired = true, channels = { ModularForceFieldSystem.CHANNEL })
+public class ModularForceFieldSystem
+{
+	/**
+	 * General Variable Definition
+	 */
+	public static final String CHANNEL = "MFFS";
+	public static final String ID = "ModularForceFieldSystem";
+	public static final String NAME = "Modular Force Field System";
+	public static final String PREFIX = "mffs:";
+	public static final String VERSION = "3.0.0";
+
+	@Instance(ModularForceFieldSystem.ID)
+	public static ModularForceFieldSystem instance;
+	public static final Logger LOGGER = Logger.getLogger(NAME);
+
+	/**
+	 * Directories Definition
+	 */
+	public static final String RESOURCE_DIRECTORY = "/mods/mffs/";
+	public static final String TEXTURE_DIRECTORY = RESOURCE_DIRECTORY + "textures/";
+	public static final String BLOCK_DIRECTORY = TEXTURE_DIRECTORY + "blocks/";
+	public static final String ITEM_DIRECTORY = TEXTURE_DIRECTORY + "items/";
+	public static final String MODEL_DIRECTORY = TEXTURE_DIRECTORY + "models/";
+	public static final String GUI_DIRECTORY = TEXTURE_DIRECTORY + "gui/";
+	public static final String GUI_BASE_DIRECTORY = GUI_DIRECTORY + "gui_base.png";
+	public static final String GUI_COMPONENTS = GUI_DIRECTORY + "gui_components.png";
+	public static final String GUI_BUTTON = GUI_DIRECTORY + "gui_button.png";
+
+}
