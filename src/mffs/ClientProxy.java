40d3937bd0326fa30461bc232d55c932915096bf
Henry Mao <admin@calclavia.com>
2013-04-13 00:38:42 +0800

    Added beam fx

--- src/mffs/ClientProxy.java
+++ src/mffs/ClientProxy.java
@@ -1,6 +1,7 @@
 package mffs;
 
 import mffs.gui.GuiFortronCapacitor;
+import mffs.render.FXBeam;
 import mffs.render.RenderBlockHandler;
 import mffs.render.RenderFortronCapacitor;
 import mffs.tileentity.TileEntityFortronCapacitor;
@@ -8,6 +9,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import universalelectricity.core.vector.Vector3;
 import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
@@ -49,5 +51,11 @@ public class ClientProxy extends CommonProxy
 		}
 
 		return null;
+	}
+
+	@Override
+	public void renderBeam(World world, Vector3 position, Vector3 target, float red, float green, float blue, int age)
+	{
+		FMLClientHandler.instance().getClient().effectRenderer.addEffect(new FXBeam(world, position, target, red, green, blue, age));
 	}
 }
