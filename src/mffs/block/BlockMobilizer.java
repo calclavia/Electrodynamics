8c2598f91e25fcbbb0fb20c2e91ab95ee5999aa3
Henry Mao <admin@calclavia.com>
2013-05-24 21:18:13 +0800

    Started working on the mobilizer

---
+++ src/mffs/block/BlockMobilizer.java
@@ -0,0 +1,50 @@
+package mffs.block;
+
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+
+public class BlockMobilizer extends BlockMachineBlock
+{
+	public BlockMobilizer(int i)
+	{
+		super(i, "mobilizer");
+	}
+
+	public static int determineOrientation(World world, int x, int y, int z, EntityPlayer entityPlayer)
+	{
+		if (MathHelper.abs((float) entityPlayer.posX - (float) x) < 2.0F && MathHelper.abs((float) entityPlayer.posZ - (float) z) < 2.0F)
+		{
+			double var5 = entityPlayer.posY + 1.82D - (double) entityPlayer.yOffset;
+
+			if (var5 - (double) y > 2.0D)
+			{
+				return 1;
+			}
+
+			if ((double) y - var5 > 0.0D)
+			{
+				return 0;
+			}
+		}
+
+		int var7 = MathHelper.floor_double((double) (entityPlayer.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+		return var7 == 0 ? 2 : (var7 == 1 ? 5 : (var7 == 2 ? 3 : (var7 == 3 ? 4 : 0)));
+	}
+
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLiving par5EntityLiving, ItemStack stack)
+	{
+		int metadata = determineOrientation(world, x, y, z, (EntityPlayer) par5EntityLiving);
+		world.setBlockMetadataWithNotify(x, y, z, metadata, 2);
+	}
+
+	@Override
+	public TileEntity createNewTileEntity(World world)
+	{
+		return new TileEntityMobilizer();
+	}
+}
---
+++ src/mffs/block/TileEntityMobilizer.java
@@ -0,0 +1,107 @@
+package mffs.block;
+
+import java.util.Iterator;
+import java.util.List;
+
+import mffs.base.TileEntityModuleAcceptor;
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraftforge.common.ForgeDirection;
+import universalelectricity.core.vector.Vector3;
+
+public class TileEntityMobilizer extends TileEntityModuleAcceptor
+{
+	@Override
+	public void updateEntity()
+	{
+		super.updateEntity();
+
+		if (this.isActive())
+		{
+			this.updatePushedObjects(1, 0.25f);
+
+			if (!this.worldObj.isRemote)
+			{
+				ForgeDirection dir = this.getDirection(this.worldObj, this.xCoord, this.yCoord, this.zCoord);
+				this.moveBlock(new Vector3(this).modifyPositionFromSide(dir), dir);
+			}
+		}
+	}
+
+	protected void moveBlock(Vector3 position, ForgeDirection direction)
+	{
+		Vector3 newPosition = position.clone().modifyPositionFromSide(direction);
+
+		TileEntity tileEntity = position.getTileEntity(this.worldObj);
+		int blockID = position.getBlockID(this.worldObj);
+		int blockMetadata = position.getBlockMetadata(this.worldObj);
+
+		if (blockID > 0 && newPosition.getBlockID(this.worldObj) == 0)
+		{
+			if (tileEntity != null)
+			{
+				NBTTagCompound tileData = new NBTTagCompound();
+				tileEntity.writeToNBT(tileData);
+				this.worldObj.removeBlockTileEntity(position.intX(), position.intY(), position.intZ());
+				position.setBlock(this.worldObj, 0);
+				newPosition.setBlock(this.worldObj, blockID);
+				this.worldObj.setBlockMetadataWithNotify(newPosition.intX(), newPosition.intY(), newPosition.intZ(), blockMetadata, 2);
+				TileEntity newTile = newPosition.getTileEntity(this.worldObj);
+				newTile.readFromNBT(tileData);
+				newTile.xCoord = position.intX();
+				newTile.yCoord = position.intY();
+				newTile.zCoord = position.intZ();
+				this.worldObj.setBlockTileEntity(position.intX(), position.intY(), position.intZ(), newTile);
+			}
+			else
+			{
+				position.setBlock(this.worldObj, 0);
+				newPosition.setBlock(this.worldObj, blockID, blockMetadata);
+			}
+		}
+	}
+
+	private void updatePushedObjects(float distance, float amount)
+	{
+		ForgeDirection dir = this.getDirection(this.worldObj, this.xCoord, this.yCoord, this.zCoord);
+		AxisAlignedBB axisalignedbb = this.getSearchAxisAlignedBB(distance, dir.ordinal());
+
+		if (axisalignedbb != null)
+		{
+			List<Entity> list = this.worldObj.getEntitiesWithinAABBExcludingEntity(null, axisalignedbb);
+
+			Iterator<Entity> iterator = list.iterator();
+
+			while (iterator.hasNext())
+			{
+				Entity entity = iterator.next();
+				entity.moveEntity((double) (amount * dir.offsetX), (double) (amount * dir.offsetY), (double) (amount * dir.offsetZ));
+			}
+		}
+	}
+
+	public AxisAlignedBB getSearchAxisAlignedBB(float distance, int direction)
+	{
+		AxisAlignedBB axisalignedbb = this.getBlockType().getCollisionBoundingBoxFromPool(worldObj, xCoord, yCoord, zCoord);
+
+		if (axisalignedbb == null)
+		{
+			return null;
+		}
+		else
+		{
+			axisalignedbb.maxY += distance;
+			return axisalignedbb;
+		}
+
+	}
+
+	@Override
+	public int getSizeInventory()
+	{
+		return 0;
+	}
+
+}
