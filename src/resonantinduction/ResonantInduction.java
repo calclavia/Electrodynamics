d4cf9300c6c7618a62132b6f5fee665599db0be2
Calclavia <admin@calclavia.com>
2013-08-01 20:29:44 -0400

    More work on base class

--- src/resonantinduction/ResonantInduction.java
+++ src/resonantinduction/ResonantInduction.java
@@ -2,11 +2,16 @@ package resonantinduction;
 
 import java.io.File;
 
+import net.minecraft.block.Block;
 import net.minecraftforge.common.Configuration;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 /**
  * @author Calclavia
@@ -53,4 +58,44 @@ public class ResonantInduction
 	 */
 	public static final Configuration CONFIGURATION = new Configuration(new File(Loader.instance().getConfigDir(), NAME + ".cfg"));
 
+	/** Block ID by Jyzarc */
+	private static final int BLOCK_ID_PREFIX = 3200;
+	/** Item ID by Horfius */
+	private static final int ITEM_ID_PREFIX = 20150;
+
+	private static int NEXT_BLOCK_ID = BLOCK_ID_PREFIX;
+	private static int NEXT_ITEM_ID = ITEM_ID_PREFIX;
+
+	public static int getNextBlockID()
+	{
+		return NEXT_BLOCK_ID++;
+	}
+
+	public static int getNextItemID()
+	{
+		return NEXT_ITEM_ID++;
+	}
+
+	/**
+	 * Blocks and Items
+	 */
+	public static Block blockTesla;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent evt)
+	{
+
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent evt)
+	{
+
+	}
+
+	@EventHandler
+	public void preInit(FMLPostInitializationEvent evt)
+	{
+
+	}
 }
