663d697a257804bf1948c23960ea4a3c958fffe0
Calclavia <admin@calclavia.com>
2013-08-01 20:17:42 -0400

    Started basic mod class

---
+++ src/resonantinduction/ResonantInduction.java
@@ -0,0 +1,21 @@
+package resonantinduction;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * @author Calclavia
+ * 
+ */
+@Mod(modid = ResonantInduction.ID, name = ResonantInduction.NAME, version = ResonantInduction.VERSION)
+public class ResonantInduction
+{
+	public static final String ID = "ResonantInduction";
+	public static final String NAME = "Resonant Induction";
+
+	public static final String MAJOR_VERSION = "@MAJOR@";
+	public static final String MINOR_VERSION = "@MINOR@";
+	public static final String REVISION_VERSION = "@REVIS@";
+	public static final String BUILD_VERSION = "@BUILD@";
+	public static final String VERSION = MAJOR_VERSION + "." + MINOR_VERSION + "." + REVISION_VERSION;
+
+}
