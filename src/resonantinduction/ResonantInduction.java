6770d6d02b8cce93d782645a2bddba94ddd669e7
Calclavia <admin@calclavia.com>
2013-08-01 21:15:02 -0400

    Added tesla middile model

--- src/resonantinduction/ResonantInduction.java
+++ src/resonantinduction/ResonantInduction.java
@@ -62,11 +62,11 @@ public class ResonantInduction
 	public static final String DIRECTORY = "/assets/" + DOMAIN + "/";
 	public static final String TEXTURE_DIRECTORY = "textures/";
 	public static final String GUI_DIRECTORY = TEXTURE_DIRECTORY + "/gui";
-	public static final String BLOCK_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "/blocks";
-	public static final String ITEM_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "/items";
-	public static final String MODEL_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "/models";
+	public static final String BLOCK_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "blocks/";
+	public static final String ITEM_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "items/";
+	public static final String MODEL_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "models/";
 
-	public static final String LANGUAGE_DIRECTORY = TEXTURE_DIRECTORY + "/models";
+	public static final String LANGUAGE_DIRECTORY = TEXTURE_DIRECTORY + "/languages";
 	public static final String[] LANGUAGES = new String[] { "en_US" };
 
 	/**
--- src/resonantinduction/render/RenderTesla.java
+++ src/resonantinduction/render/RenderTesla.java
@@ -5,10 +5,13 @@ package resonantinduction.render;
 
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
 
 import org.lwjgl.opengl.GL11;
 
+import resonantinduction.ResonantInduction;
 import resonantinduction.model.ModelTeslaBottom;
+import resonantinduction.model.ModelTeslaMiddle;
 
 /**
  * @author Calclavia
@@ -16,7 +19,11 @@ import resonantinduction.model.ModelTeslaBottom;
  */
 public class RenderTesla extends TileEntitySpecialRenderer
 {
-	public static final ModelTeslaBottom MODEL = new ModelTeslaBottom();
+	public static final ResourceLocation TEXTURE_BOTTOM = new ResourceLocation(ResonantInduction.DOMAIN, ResonantInduction.MODEL_TEXTURE_DIRECTORY + "tesla_bottom.png");
+	public static final ResourceLocation TEXTURE_MIDDILE = new ResourceLocation(ResonantInduction.DOMAIN, ResonantInduction.MODEL_TEXTURE_DIRECTORY + "tesla_middile.png");
+	public static final ResourceLocation TEXTURE_TOP = new ResourceLocation(ResonantInduction.DOMAIN, ResonantInduction.MODEL_TEXTURE_DIRECTORY + "tesla_bottom.png");
+	public static final ModelTeslaBottom MODEL_BOTTOM = new ModelTeslaBottom();
+	public static final ModelTeslaMiddle MODEL_MIDDILE = new ModelTeslaMiddle();
 
 	@Override
 	public void renderTileEntityAt(TileEntity t, double x, double y, double z, float f)
@@ -24,7 +31,8 @@ public class RenderTesla extends TileEntitySpecialRenderer
 		GL11.glPushMatrix();
 		GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
 		GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
-		MODEL.render(0.0625f);
+		this.func_110628_a(TEXTURE_BOTTOM);
+		MODEL_BOTTOM.render(0.0625f);
 		GL11.glPopMatrix();
 	}
 
