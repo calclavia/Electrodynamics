10dabcc941adc39b37743b67dec1db4830856e07
Calclavia <admin@calclavia.com>
2013-08-01 20:24:57 -0400

    Updated build script

--- src/resonantinduction/ResonantInduction.java
+++ src/resonantinduction/ResonantInduction.java
@@ -1,5 +1,9 @@
 package resonantinduction;
 
+import java.io.File;
+
+import net.minecraftforge.common.Configuration;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.ModMetadata;
@@ -11,6 +15,9 @@ import cpw.mods.fml.common.ModMetadata;
 @Mod(modid = ResonantInduction.ID, name = ResonantInduction.NAME, version = ResonantInduction.VERSION)
 public class ResonantInduction
 {
+	/**
+	 * Mod Information
+	 */
 	public static final String ID = "ResonantInduction";
 	public static final String NAME = "Resonant Induction";
 
@@ -25,4 +32,25 @@ public class ResonantInduction
 
 	@Mod.Metadata(ID)
 	public static ModMetadata metadata;
+
+	/**
+	 * Directory Information
+	 */
+	public static final String DOMAIN = "resonantinduction";
+	public static final String PREFIX = DOMAIN + ":";
+	public static final String DIRECTORY = "/assets/" + DOMAIN + "/";
+	public static final String TEXTURE_DIRECTORY = "textures/";
+	public static final String GUI_DIRECTORY = TEXTURE_DIRECTORY + "/gui";
+	public static final String BLOCK_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "/blocks";
+	public static final String ITEM_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "/items";
+	public static final String MODEL_TEXTURE_DIRECTORY = TEXTURE_DIRECTORY + "/models";
+
+	public static final String LANGUAGE_DIRECTORY = TEXTURE_DIRECTORY + "/models";
+	public static final String[] LANGUAGES = new String[] { "en_US" };
+
+	/**
+	 * Settings
+	 */
+	public static final Configuration CONFIGURATION = new Configuration(new File(Loader.instance().getConfigDir(), NAME + ".cfg"));
+
 }
