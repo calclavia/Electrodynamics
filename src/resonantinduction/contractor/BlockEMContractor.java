f27aaa3c4ba49bda41befe3b83c2220d08267af1
Aidan Brady <aidancbrady@aol.com>
2013-08-02 23:59:39 -0400

    PacketHandler!

--- src/resonantinduction/contractor/BlockEMContractor.java
+++ src/resonantinduction/contractor/BlockEMContractor.java
@@ -30,7 +30,7 @@ public class BlockEMContractor extends BlockBase implements ITileEntityProvider
 	@Override
 	public boolean onBlockActivated(World par1World, int par2, int par3, int par4, EntityPlayer par5EntityPlayer, int par6, float par7, float par8, float par9)
     {
-		if(par1World.isRemote)
+		if(par1World.isRemote && !par5EntityPlayer.isSneaking())
 		{
 			TileEntityEMContractor contractor = (TileEntityEMContractor)par1World.getBlockTileEntity(par2, par3, par4);
 			contractor.incrementFacing();
--- src/resonantinduction/contractor/TileEntityEMContractor.java
+++ src/resonantinduction/contractor/TileEntityEMContractor.java
@@ -7,6 +7,12 @@ public class TileEntityEMContractor extends TileEntity
 {
 	public ForgeDirection facing = ForgeDirection.UP;
 	
+	@Override
+	public void updateEntity()
+	{
+		
+	}
+	
 	public void incrementFacing()
 	{
 		int newOrdinal = facing.ordinal() < 5 ? facing.ordinal()+1 : 0;
