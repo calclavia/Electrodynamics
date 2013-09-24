9df402ea964255145c73497ddc7ecbd97ab8f1ef
Ben Spiers <ben.spiers22@gmail.com>
2013-09-24 03:09:13 +0100

    Refine some metal colours to make the grey metals a bit more different and the others look a bit better (I think).

--- src/resonantinduction/wire/EnumWireMaterial.java
+++ src/resonantinduction/wire/EnumWireMaterial.java
@@ -12,12 +12,12 @@ import universalelectricity.core.vector.Vector3;
 
 public enum EnumWireMaterial
 {
-	COPPER(12.5f, 3, 2, new Vector3(132, 73, 33)),
+	COPPER(12.5f, 3, 2, new Vector3(184, 115, 51)),
 	TIN(13, 2, 0.5f, new Vector3(215, 205, 181)),
-	IRON(0.1f, 2, 4, new Vector3(97, 102, 105)),
+	IRON(0.1f, 2, 4, new Vector3(132, 132, 130)),
 	ALUMINUM(0.025f, 6, 0.15f, new Vector3(97, 102, 105)),
-	SILVER(0.005f, 1, 2, new Vector3(176, 175, 171)),
-	SUPERCONDUCTOR(0, 8, Float.MAX_VALUE, new Vector3(242, 210, 63));
+	SILVER(0.005f, 1, 2, new Vector3(255, 255, 255)),
+	SUPERCONDUCTOR(0, 8, Float.MAX_VALUE, new Vector3(212, 175, 55));
 
 	public final float resistance;
 	public final float damage;
