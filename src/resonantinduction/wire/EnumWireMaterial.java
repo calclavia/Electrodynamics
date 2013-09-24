db9acd9058d47c215b9b329b38cfef785764ecc9
Ben Spiers <ben.spiers22@gmail.com>
2013-09-24 04:12:49 +0100

    Realise why increasing the "V" in HSV seemed to be having no effect, feel silly.
    It would be cool if we could do some sort of specular reflection on the silver and maybe superconductor wires, but I don't think we can. Or if we could it would be way beyond the scope of this mod.

--- src/resonantinduction/wire/EnumWireMaterial.java
+++ src/resonantinduction/wire/EnumWireMaterial.java
@@ -13,10 +13,10 @@ import universalelectricity.core.vector.Vector3;
 public enum EnumWireMaterial
 {
 	COPPER(12.5f, 3, 2, new Vector3(184, 115, 51)),
-	TIN(13, 2, 0.5f, new Vector3(215, 205, 181)),
-	IRON(0.1f, 2, 4, new Vector3(132, 132, 130)),
-	ALUMINUM(0.025f, 6, 0.15f, new Vector3(97, 102, 105)),
-	SILVER(0.005f, 1, 2, new Vector3(255, 255, 255)),
+	TIN(13, 2, 0.5f, new Vector3(132, 132, 130)),
+	IRON(0.1f, 2, 4, new Vector3(97, 102, 105)),
+	ALUMINUM(0.025f, 6, 0.15f, new Vector3(215, 205, 181)),
+	SILVER(0.005f, 1, 2, new Vector3(192, 192, 192)),
 	SUPERCONDUCTOR(0, 8, Float.MAX_VALUE, new Vector3(212, 175, 55));
 
 	public final float resistance;
@@ -29,6 +29,6 @@ public enum EnumWireMaterial
 		this.resistance = resistance;
 		this.damage = electrocutionDamage;
 		this.maxAmps = maxAmps;
-		this.color = color.normalize();
+		this.color = color.scale(1D/255D);
 	}
 }
