ba6a56ab7e4e63f7776ec1c27a2ffcc5afeeea85
Calclavia <admin@calclavia.com>
2014-01-05 14:26:59 +0800

    Added fluid mixture NBT saving

--- src/main/java/resonantinduction/machine/liquid/TileFluidMixture.java
+++ src/main/java/resonantinduction/machine/liquid/TileFluidMixture.java
@@ -1,12 +1,52 @@
 package resonantinduction.machine.liquid;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraftforge.fluids.FluidStack;
 import calclavia.lib.prefab.tile.TileAdvanced;
 
 /**
  * @author Calclavia
- *
+ * 
  */
 public class TileFluidMixture extends TileAdvanced
 {
+	public final Set<FluidStack> fluids = new HashSet<FluidStack>();
 
+	@Override
+	public void readFromNBT(NBTTagCompound nbt)
+	{
+		super.readFromNBT(nbt);
+
+		fluids.clear();
+
+		NBTTagList nbtList = nbt.getTagList("fluids");
+
+		for (int i = 0; i < nbtList.tagCount(); ++i)
+		{
+			NBTTagCompound fluidNBT = (NBTTagCompound) nbtList.tagAt(i);
+			fluids.add(FluidStack.loadFluidStackFromNBT(fluidNBT));
+		}
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound nbt)
+	{
+		super.writeToNBT(nbt);
+
+		NBTTagList nbtList = new NBTTagList();
+
+		for (FluidStack fluid : fluids)
+		{
+			NBTTagCompound nbtElement = new NBTTagCompound();
+			fluid.writeToNBT(nbtElement);
+			nbtList.appendTag(nbtElement);
+		}
+
+		nbt.setTag("fluids", nbtList);
+	}
 }
