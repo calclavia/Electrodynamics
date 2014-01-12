a9b373e639148070860c5b53c3b3a8d0de415c1f
DarkGuardsman <rseifert.phone@gmail.com>
2014-01-12 02:40:36 -0500

    Fixed pipe & tank placement issue

--- src/main/java/resonantinduction/mechanical/Mechanical.java
+++ src/main/java/resonantinduction/mechanical/Mechanical.java
@@ -7,6 +7,7 @@ import resonantinduction.core.Settings;
 import resonantinduction.mechanical.belt.BlockConveyorBelt;
 import resonantinduction.mechanical.belt.TileConveyorBelt;
 import resonantinduction.mechanical.fluid.pipe.BlockPipe;
+import resonantinduction.mechanical.fluid.pipe.ItemBlockFluidContainer;
 import resonantinduction.mechanical.fluid.pipe.TilePipe;
 import resonantinduction.mechanical.fluid.pump.BlockGrate;
 import resonantinduction.mechanical.fluid.pump.BlockPump;
@@ -26,60 +27,57 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkRegistry;
 
-/**
- * Resonant Induction Archaic Module
+/** Resonant Induction Archaic Module
  * 
- * @author DarkCow, Calclavia
- * 
- */
+ * @author DarkCow, Calclavia */
 @Mod(modid = Mechanical.ID, name = Mechanical.NAME, version = Reference.VERSION, dependencies = "required-after:" + ResonantInduction.ID)
 @NetworkMod(channels = Reference.CHANNEL, clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class Mechanical
 {
-	/** Mod Information */
-	public static final String ID = "ResonantInduction|Mechanical";
-	public static final String NAME = Reference.NAME + " Mechanical";
+    /** Mod Information */
+    public static final String ID = "ResonantInduction|Mechanical";
+    public static final String NAME = Reference.NAME + " Mechanical";
 
-	@Instance(ID)
-	public static Mechanical INSTANCE;
+    @Instance(ID)
+    public static Mechanical INSTANCE;
 
-	@SidedProxy(clientSide = "resonantinduction.mechanical.ClientProxy", serverSide = "resonantinduction.mechanical.CommonProxy")
-	public static CommonProxy proxy;
+    @SidedProxy(clientSide = "resonantinduction.mechanical.ClientProxy", serverSide = "resonantinduction.mechanical.CommonProxy")
+    public static CommonProxy proxy;
 
-	@Mod.Metadata(ID)
-	public static ModMetadata metadata;
+    @Mod.Metadata(ID)
+    public static ModMetadata metadata;
 
-	public static final ContentRegistry contentRegistry = new ContentRegistry(Settings.CONFIGURATION, ID);
+    public static final ContentRegistry contentRegistry = new ContentRegistry(Settings.CONFIGURATION, ID);
 
-	// Transport
-	public static Block blockConveyorBelt;
+    // Transport
+    public static Block blockConveyorBelt;
 
-	// #Fluids
-	public static Block blockTank;
-	public static Block blockPipe;
-	public static Block blockReleaseValve;
-	public static Block blockGrate;
-	public static Block blockPump;
+    // #Fluids
+    public static Block blockTank;
+    public static Block blockPipe;
+    public static Block blockReleaseValve;
+    public static Block blockGrate;
+    public static Block blockPump;
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent evt)
-	{
-	    Settings.load();
-		NetworkRegistry.instance().registerGuiHandler(this, proxy);
-		blockConveyorBelt = contentRegistry.createTile(BlockConveyorBelt.class, TileConveyorBelt.class);
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent evt)
+    {
+        Settings.load();
+        NetworkRegistry.instance().registerGuiHandler(this, proxy);
+        blockConveyorBelt = contentRegistry.createTile(BlockConveyorBelt.class, TileConveyorBelt.class);
 
-		blockTank = contentRegistry.createTile(BlockTank.class, TileTank.class);
-		blockPipe = contentRegistry.createTile(BlockPipe.class, TilePipe.class);
-		blockGrate = contentRegistry.createTile(BlockGrate.class, TileGrate.class);
-		blockPump = contentRegistry.createTile(BlockPump.class, TilePump.class);
-		proxy.preInit();
-		Settings.save();
-	}
+        blockTank = contentRegistry.createBlock(BlockTank.class, ItemBlockFluidContainer.class, TileTank.class);
+        blockPipe = contentRegistry.createBlock(BlockPipe.class, ItemBlockFluidContainer.class, TilePipe.class);
+        blockGrate = contentRegistry.createTile(BlockGrate.class, TileGrate.class);
+        blockPump = contentRegistry.createTile(BlockPump.class, TilePump.class);
+        proxy.preInit();
+        Settings.save();
+    }
 
-	@EventHandler
-	public void init(FMLInitializationEvent evt)
-	{
-		Settings.setModMetadata(metadata, ID, NAME);
-		proxy.init();
-	}
+    @EventHandler
+    public void init(FMLInitializationEvent evt)
+    {
+        Settings.setModMetadata(metadata, ID, NAME);
+        proxy.init();
+    }
 }
