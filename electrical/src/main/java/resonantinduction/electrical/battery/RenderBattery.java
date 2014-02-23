55458ff82a1a8ebda3514b359ddfd2b0878bb815
Calclavia <admin@calclavia.com>
2014-02-23 14:26:05 +0800

    Added new awesome battery model thanks to CyanideX!

--- electrical/src/main/java/resonantinduction/electrical/battery/RenderBattery.java
+++ electrical/src/main/java/resonantinduction/electrical/battery/RenderBattery.java
@@ -8,14 +8,19 @@ import static org.lwjgl.opengl.GL11.glPushMatrix;
 import static org.lwjgl.opengl.GL11.glRotatef;
 import static org.lwjgl.opengl.GL11.glScalef;
 import static org.lwjgl.opengl.GL11.glTranslatef;
+
+import javax.naming.directory.DirContext;
+
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
 import net.minecraftforge.client.model.obj.WavefrontObject;
 import net.minecraftforge.common.ForgeDirection;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.lwjgl.opengl.GL11;
 
 import resonantinduction.core.Reference;
@@ -34,135 +39,56 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class RenderBattery extends TileEntitySpecialRenderer implements ISimpleItemRenderer
 {
 	public static RenderBattery INSTANCE = new RenderBattery();
+	public static final IModelCustom MODEL = AdvancedModelLoader.loadModel(Reference.MODEL_DIRECTORY + "battery/battery.tcn");
 
-	public static final ResourceLocation TEXTURE_CAP = new ResourceLocation(Reference.DOMAIN, Reference.MODEL_PATH + "battery/bat_base_cap_tex.png");
-	public static final ResourceLocation TEXTURE_CASE = new ResourceLocation(Reference.DOMAIN, Reference.MODEL_PATH + "battery/bat_case_tex.png");
-	public static final WavefrontObject MODEL = (WavefrontObject) AdvancedModelLoader.loadModel(Reference.MODEL_DIRECTORY + "battery/battery.obj");
+	//Renders only if another battery on this side exists
+	public static String[][] renderForSideOnly = new String[][] { new String[] {  }, new String[] {  }, new String[] { "frame1", "frame2" }, new String[] { "frame2", "frame3" }, new String[] { "frame3", "frame4" }, new String[] { "frame4", "frame5" } };
+	
+	//Renders only if another battery on this side does NOT exist
+	public static String[][] renderNotForSide = new String[][] { new String[] {"bottom", "coil1"}, new String[] { "top", "frame1con", "frame2con", "frame3con", "frame4con" }, new String[] { }, new String[] {  }, new String[] {  }, new String[] {  } };
 
 	@Override
 	public void renderInventoryItem(ItemStack itemStack)
 	{
 		glPushMatrix();
-		GL11.glTranslated(0, -0.5f, 0);
-
-		for (int i = 2; i < 6; i++)
-		{
-			glPushMatrix();
-			glScalef(0.5f, 0.5f, 0.5f);
-			ForgeDirection dir = ForgeDirection.getOrientation(i);
-
-			switch (dir)
-			{
-				case NORTH:
-					glRotatef(0, 0, 1, 0);
-					break;
-				case SOUTH:
-					glRotatef(180, 0, 1, 0);
-					break;
-				case WEST:
-					glRotatef(90, 0, 1, 0);
-					break;
-				case EAST:
-					glRotatef(-90, 0, 1, 0);
-					break;
-			}
-
-			int energyLevel = (int) (((double) ((ItemBlockBattery) itemStack.getItem()).getEnergy(itemStack) / (double) ((ItemBlockBattery) itemStack.getItem()).getEnergyCapacity(itemStack)) * 10);
-			RenderUtility.bind(Reference.DOMAIN, Reference.MODEL_PATH + "battery/bat_level_" + energyLevel + ".png");
-			MODEL.renderPart("Battery");
-			FMLClientHandler.instance().getClient().renderEngine.bindTexture(TEXTURE_CAP);
-			MODEL.renderOnly("CapCorner", "BaseCorner", "CapEdge", "BaseEdge", "CapInterior", "BaseInterior");
-			FMLClientHandler.instance().getClient().renderEngine.bindTexture(TEXTURE_CASE);
-			MODEL.renderOnly("BatteryCase");
-			glPopMatrix();
-		}
-
+		GL11.glTranslated(0, 0, 0);
+		int energyLevel = (int) (((double) ((ItemBlockBattery) itemStack.getItem()).getEnergy(itemStack) / (double) ((ItemBlockBattery) itemStack.getItem()).getEnergyCapacity(itemStack)) * 8);
+		RenderUtility.bind(Reference.DOMAIN, Reference.MODEL_PATH + "battery/battery_" + energyLevel + ".png");
+		MODEL.renderAllExcept("frame1con", "frame2con", "frame3con", "frame4con");
 		glPopMatrix();
 	}
 
 	@Override
 	public void renderTileEntityAt(TileEntity t, double x, double y, double z, float f)
 	{
-		glPushMatrix();
-		glTranslatef((float) x + 0.5F, (float) y, (float) z + 0.5F);
+	
+		renderForSideOnly = new String[][] { new String[] { "frame1con", "frame2con", "frame3con", "frame4con" }, new String[] {  }, new String[] { }, new String[] {  }, new String[] { }, new String[] {  } };
+		renderNotForSide = new String[][] { new String[] {"bottom", "coil1"}, new String[] { "top" }, new String[] { "frame1" }, new String[] { "frame3"  }, new String[] { "frame2" }, new String[] { "frame4" } };
 
-		for (int i = 2; i < 6; i++)
+		
+		String[][] 	partToDisable = new String[][] { new String[] {"bottom", "coil1"}, new String[] { "top","frame1con" ,"frame2con" ,"frame3con" ,"frame4con" }, new String[] {  }, new String[] {   }, new String[] {  }, new String[] {  } };
+
+		GL11.glPushMatrix();
+		GL11.glTranslated(x + 0.5, y + 0.5, z + 0.5);
+
+		TileBattery tile = (TileBattery) t;
+
+		int energyLevel = (int) (((double) tile.energy.getEnergy() / (double) TileBattery.getEnergyForTier(tile.getBlockMetadata())) * 8);
+		RenderUtility.bind(Reference.DOMAIN, Reference.MODEL_PATH + "battery/battery_" + energyLevel + ".png");
+
+		String[] disabledParts = new String[0];
+		
+		for (ForgeDirection check : ForgeDirection.VALID_DIRECTIONS)
 		{
-			glPushMatrix();
-			ForgeDirection dir = ForgeDirection.getOrientation(i);
-
-			switch (dir)
+			if (new Vector3(t).translate(check).getTileEntity(t.worldObj) instanceof TileBattery)
 			{
-				case NORTH:
-					glRotatef(0, 0, 1, 0);
-					break;
-				case SOUTH:
-					glRotatef(180, 0, 1, 0);
-					break;
-				case WEST:
-					glRotatef(90, 0, 1, 0);
-					break;
-				case EAST:
-					glRotatef(-90, 0, 1, 0);
-					break;
+				disabledParts = ArrayUtils.addAll(disabledParts, partToDisable[check.ordinal()]);
 			}
-
-			glScalef(0.5f, 0.5f, 0.5f);
-
-			/**
-			 * If we're rendering in the world:
-			 */
-			TileBattery tile = (TileBattery) t;
-
-			int energyLevel = (int) (((double) tile.energy.getEnergy() / (double) TileBattery.getEnergyForTier(tile.getBlockMetadata())) * 10);
-			RenderUtility.bind(Reference.DOMAIN, Reference.MODEL_PATH + "battery/bat_level_" + energyLevel + ".png");
-			MODEL.renderPart("Battery");
-
-			// Render top and bottom
-			FMLClientHandler.instance().getClient().renderEngine.bindTexture(TEXTURE_CAP);
-			if (!(new Vector3(t).translate(ForgeDirection.UP).getTileEntity(t.worldObj) instanceof TileBattery))
-				MODEL.renderPart("CapCorner");
-			if (!(new Vector3(t).translate(ForgeDirection.DOWN).getTileEntity(t.worldObj) instanceof TileBattery))
-				MODEL.renderPart("BaseCorner");
-
-			// If quadrant with one external neighbor
-			FMLClientHandler.instance().getClient().renderEngine.bindTexture(TEXTURE_CAP);
-
-			if (!(new Vector3(t).translate(ForgeDirection.UP).getTileEntity(t.worldObj) instanceof TileBattery))
-				MODEL.renderPart("CapEdge");
-			if (!(new Vector3(t).translate(ForgeDirection.DOWN).getTileEntity(t.worldObj) instanceof TileBattery))
-				MODEL.renderPart("BaseEdge");
-
-			/*
-			 * If quadrant with three external neighbors //can't have quadrant with 2 external
-			 * neighbors in rectangular prism
-			 */
-			if (!(new Vector3(t).translate(ForgeDirection.UP).getTileEntity(t.worldObj) instanceof TileBattery))
-				MODEL.renderPart("CapInterior");
-			if (!(new Vector3(t).translate(ForgeDirection.DOWN).getTileEntity(t.worldObj) instanceof TileBattery))
-				MODEL.renderPart("BaseInterior");
-
-			FMLClientHandler.instance().getClient().renderEngine.bindTexture(TEXTURE_CASE);
-
-			if (tile.getInputDirections().contains(dir))
-			{
-				GL11.glColor3f(0, 0.294f, 0.498f);
-			}
-			else if (tile.getOutputDirections().contains(dir))
-			{
-				GL11.glColor3f(1, 0.478f, 0.01f);
-			}
-
-			MODEL.renderPart("BatteryCase");
-
-			GL11.glColor3f(1, 1, 1);
-
-			if (new Vector3(t).translate(ForgeDirection.UP).getTileEntity(t.worldObj) instanceof TileBattery)
-				MODEL.renderPart("VertConnector");
-
-			glPopMatrix();
 		}
 
-		glPopMatrix();
+		GL11.glColor3f(1, 1, 1);
+		MODEL.renderAllExcept(disabledParts);
+
+		GL11.glPopMatrix();
 	}
 }
