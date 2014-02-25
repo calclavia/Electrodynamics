47b845a67ad38f9549e502d2be492672989f9185
Calclavia <admin@calclavia.com>
2014-02-25 22:59:57 +0800

    Fixed solar panel and thermopile texture

--- electrical/src/main/java/resonantinduction/electrical/generator/thermopile/BlockThermopile.java
+++ electrical/src/main/java/resonantinduction/electrical/generator/thermopile/BlockThermopile.java
@@ -1,6 +1,10 @@
 package resonantinduction.electrical.generator.thermopile;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import resonantinduction.core.Reference;
 import universalelectricity.api.UniversalElectricity;
@@ -8,9 +12,31 @@ import calclavia.lib.prefab.block.BlockTile;
 
 public class BlockThermopile extends BlockTile
 {
+	public Icon topIcon;
+
 	public BlockThermopile(int id)
 	{
 		super(id, UniversalElectricity.machine);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerIcons(IconRegister iconReg)
+	{
+		topIcon = iconReg.registerIcon(Reference.PREFIX + "thermopile_top");
+		super.registerIcons(iconReg);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public Icon getIcon(int side, int meta)
+	{
+		if (side == 1)
+		{
+			return topIcon;
+		}
+
+		return blockIcon;
 	}
 
 	@Override
