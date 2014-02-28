c27a8ecff2960283cef43a9993a2b3c91654d3ba
Calclavia <admin@calclavia.com>
2014-02-28 08:51:22 +0800

    Fixed #409 - Stabilizer now consumes item before placing

--- src/main/java/mffs/item/module/projector/ItemModuleStablize.java
+++ src/main/java/mffs/item/module/projector/ItemModuleStablize.java
@@ -12,10 +12,12 @@ import mffs.item.mode.ItemModeCustom;
 import mffs.item.module.ItemModule;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFluid;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeDirection;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fluids.IFluidBlock;
@@ -48,6 +50,8 @@ public class ItemModuleStablize extends ItemModule
 
 		if (projector.getTicks() % 40 == 0)
 		{
+			World world = ((TileEntity) projector).worldObj;
+
 			if (projector.getMode() instanceof ItemModeCustom && !(projector.getModuleCount(ModularForceFieldSystem.itemModuleCamouflage) > 0))
 			{
 				HashMap<Vector3, int[]> fieldBlocks = ((ItemModeCustom) projector.getMode()).getFieldBlockMap(projector, projector.getModeStack());
@@ -73,7 +77,7 @@ public class ItemModuleStablize extends ItemModule
 
 						if (checkStack != null)
 						{
-							EventStabilize evt = new EventStabilize(((TileEntity) projector).worldObj, position.intX(), position.intY(), position.intZ(), checkStack);
+							EventStabilize evt = new EventStabilize(world, position.intX(), position.intY(), position.intZ(), checkStack);
 							MinecraftForge.EVENT_BUS.post(evt);
 
 							if (!evt.isCanceled())
@@ -84,7 +88,7 @@ public class ItemModuleStablize extends ItemModule
 									{
 										try
 										{
-											if (((TileEntity) projector).worldObj.canPlaceEntityOnSide(((ItemBlock) checkStack.getItem()).getBlockID(), position.intX(), position.intY(), position.intZ(), false, 0, null, checkStack))
+											if (world.canPlaceEntityOnSide(((ItemBlock) checkStack.getItem()).getBlockID(), position.intX(), position.intY(), position.intZ(), false, 0, null, checkStack))
 											{
 												int metadata = blockInfo != null ? blockInfo[1] : (checkStack.getHasSubtypes() ? checkStack.getItemDamage() : 0);
 												Block block = blockInfo != null ? Block.blocksList[blockInfo[0]] : null;
@@ -94,8 +98,9 @@ public class ItemModuleStablize extends ItemModule
 													return 1;
 												}
 
-												((ItemBlock) checkStack.getItem()).placeBlockAt(checkStack, null, ((TileEntity) projector).worldObj, position.intX(), position.intY(), position.intZ(), 0, 0, 0, 0, metadata);
+												ItemStack copyStack = checkStack.copy();
 												inventory.decrStackSize(i, 1);
+												((ItemBlock) copyStack.getItem()).placeBlockAt(copyStack, null, ((TileEntity) projector).worldObj, position.intX(), position.intY(), position.intZ(), 0, 0, 0, 0, metadata);
 												PacketHandler.sendPacketToClients(ModularForceFieldSystem.PACKET_TILE.getPacket((TileEntity) projector, TilePacketType.FXS.ordinal(), 1, position.intX(), position.intY(), position.intZ()), ((TileEntity) projector).worldObj);
 
 												if (this.blockCount++ >= projector.getModuleCount(ModularForceFieldSystem.itemModuleSpeed) / 3)
@@ -110,6 +115,7 @@ public class ItemModuleStablize extends ItemModule
 										}
 										catch (Exception e)
 										{
+											ModularForceFieldSystem.LOGGER.severe("Stabilizer failed to place item '" + checkStack + "'. The item or block may not have correctly implemented the placement methods.");
 											e.printStackTrace();
 										}
 									}
