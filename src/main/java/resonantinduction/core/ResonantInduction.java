27bc8b9e47bdd18dabfe7f3cabacc88e2f36c253
Calclavia <admin@calclavia.com>
2014-03-07 22:15:33 -0800

    Fixed #390 - Threaded grid not running client side

--- src/main/java/resonantinduction/core/ResonantInduction.java
+++ src/main/java/resonantinduction/core/ResonantInduction.java
@@ -141,6 +141,9 @@ public class ResonantInduction
 		// Generate Resources
 		ResourceGenerator.generateOreResources();
 		proxy.postInit();
+		
+		if (!UpdateTicker.INSTANCE.isAlive())
+			UpdateTicker.INSTANCE.start();
 	}
 
 	@EventHandler
