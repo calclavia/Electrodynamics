d791bf81ce50b3abb6ad106e04227e3b10398533
Calclavia <admin@calclavia.com>
2014-03-14 22:43:28 +0800

    Removed unused API

--- src/main/java/mffs/api/fortron/IFortronFrequency.java
+++
@@ -1,9 +0,0 @@
-package mffs.api.fortron;
-
-import calclavia.api.icbm.IBlockFrequency;
-import calclavia.api.mffs.fortron.IFortronStorage;
-
-public interface IFortronFrequency extends IFortronStorage, IBlockFrequency
-{
-
-}
