1c87ee9858e4e354ccfe681009b71a62d7b25cc0
Calclavia <admin@calclavia.com>
2014-03-14 22:54:26 +0800

    Decreased water turbine torque

--- mechanical/src/main/java/resonantinduction/mechanical/energy/turbine/TileWaterTurbine.java
+++ mechanical/src/main/java/resonantinduction/mechanical/energy/turbine/TileWaterTurbine.java
@@ -58,7 +58,7 @@ public class TileWaterTurbine extends TileMechanicalTurbine
 		if (getMultiBlock().isConstructed())
 			torque = (long) (defaultTorque / (1d / multiBlockRadius));
 		else
-			torque = defaultTorque / 20;
+			torque = defaultTorque / 22;
 
 		/**
 		 * If this is a horizontal turbine.
@@ -89,7 +89,7 @@ public class TileWaterTurbine extends TileMechanicalTurbine
 		}
 		else
 		{
-			maxPower = 3000;
+			maxPower = 2000;
 
 			int checkX = xCoord;
 			int checkY = yCoord - 1;
