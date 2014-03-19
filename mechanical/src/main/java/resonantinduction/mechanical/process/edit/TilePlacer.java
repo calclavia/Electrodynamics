1bb8ae8828f41a9a880ed3a9196a869430f9aeb7
tgame14 <tgameBusiness@gmail.com>
2014-03-19 17:19:58 +0200

    Fixed up conflicts

--- mechanical/src/main/java/resonantinduction/mechanical/process/edit/TilePlacer.java
+++ mechanical/src/main/java/resonantinduction/mechanical/process/edit/TilePlacer.java
@@ -6,24 +6,17 @@ import calclavia.lib.network.IPacketReceiver;
 import calclavia.lib.network.PacketHandler;
 import calclavia.lib.prefab.tile.IRotatable;
 import calclavia.lib.render.RenderItemOverlayUtility;
-<<<<<<< HEAD
 import calclavia.lib.utility.inventory.InventoryUtility;
-=======
->>>>>>> 274dfa07f76b33f8ffb4a1d1967ea6d904d56dcf
 import com.google.common.io.ByteArrayDataInput;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.packet.Packet;
-<<<<<<< HEAD
 import net.minecraft.tileentity.TileEntity;
-=======
->>>>>>> 274dfa07f76b33f8ffb4a1d1967ea6d904d56dcf
 import net.minecraftforge.common.ForgeDirection;
 import org.lwjgl.opengl.GL11;
 import resonantinduction.core.ResonantInduction;
@@ -67,7 +60,6 @@ public class TilePlacer extends TileInventory implements IRotatable, IPacketRece
                 ForgeDirection op = dir.getOpposite();
                 TileEntity tile = getWorldObj().getBlockTileEntity(x() + op.offsetX, y() + op.offsetY, z() + op.offsetZ);
 
-<<<<<<< HEAD
                 if (tile instanceof IInventory)
                 {
                     ItemStack candidate = InventoryUtility.takeTopItemFromInventory((IInventory) tile, dir.ordinal());
@@ -76,30 +68,15 @@ public class TilePlacer extends TileInventory implements IRotatable, IPacketRece
                 }
             }
             placeStack = getStackInSlot(0);
-=======
-	@Override
-	protected boolean use(EntityPlayer player, int hitSide, Vector3 hit)
-	{
-		interactCurrentItem(this, 0, player);
-		return true;
-	}
 
-	public void work()
-	{
-		if (isIndirectlyPowered())
-		{
-			ForgeDirection dir = getDirection();
-			Vector3 check = position().translate(dir);
-			ItemStack placeStack = getStackInSlot(0);
->>>>>>> 274dfa07f76b33f8ffb4a1d1967ea6d904d56dcf
-
-            if (world().isAirBlock(check.intX(), check.intY(), check.intZ()) && placeStack != null && placeStack.getItem() instanceof ItemBlock)
-            {
-                ItemStack copyPlaceStack = placeStack.copy();
-                decrStackSize(0, 1);
-                ((ItemBlock) copyPlaceStack.getItem()).placeBlockAt(placeStack, null, world(), check.intX(), check.intY(), check.intZ(), 0, 0, 0, 0, copyPlaceStack.getItemDamage());
-            }
         }
+    }
+
+    @Override
+    protected boolean use (EntityPlayer player, int hitSide, Vector3 hit)
+    {
+        interactCurrentItem(this, 0, player);
+        return true;
     }
 
     @Override
@@ -143,4 +120,5 @@ public class TilePlacer extends TileInventory implements IRotatable, IPacketRece
             }
         };
     }
+}
 }
