30e1b0c1bb0756509509c5fc4a81b7aad879b09f
Calclavia <admin@calclavia.com>
2014-03-23 11:00:00 +0800

    Fixed tgame trying to give traits constructor

--- electrical/src/main/scala/resonantinduction/electrical/TileDistributor.scala
+++ electrical/src/main/scala/resonantinduction/electrical/TileDistributor.scala
@@ -2,12 +2,13 @@ package resonantinduction.electrical
 
 import calclavia.lib.content.prefab.{TraitElectrical, TraitInventory}
 import net.minecraft.block.material.Material
+import calclavia.lib.content.module.TileBase
 
 /**
  * @since 22/03/14
  * @author tgame14
  */
-class TileDistributor() extends TraitInventory(Material.rock) with TraitElectrical
+class TileDistributor extends TileBase(Material.rock) with TraitInventory with TraitElectrical
 {
 
 }
