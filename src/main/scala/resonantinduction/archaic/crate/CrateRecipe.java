1fe751f5479ffd3c071bfba86766d2a6235337d6
Robert S <rseifert.phone@gmail.com>
2014-07-22 15:55:09 -0400

    Changed crate crafting to use a recipe instead of handler object

---
+++ src/main/scala/resonantinduction/archaic/crate/CrateRecipe.java
@@ -0,0 +1,44 @@
+package resonantinduction.archaic.crate;
+
+import net.minecraft.block.Block;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.world.World;
+import resonantinduction.archaic.Archaic;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import resonantinduction.archaic.ArchaicBlocks;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+
+/** Crafting handler for crates
+ * 
+ * @author Darkguardsman */
+public class CrateRecipe extends ShapedOreRecipe implements IRecipe
+{
+    public CrateRecipe(Block result, Object... recipe){ super(new ItemStack(result), recipe); }
+    public CrateRecipe(Item result, Object... recipe){ super(new ItemStack(result), recipe); }
+    public CrateRecipe(ItemStack result, Object... recipe){super(result, recipe);}
+
+    @Override
+    public ItemStack getCraftingResult(InventoryCrafting grid)
+    {
+        ItemStack result = super.getCraftingResult(grid);
+        Item crateItem = Item.getItemFromBlock(ArchaicBlocks.blockCrate());
+
+        if (result != null && result.getItem() == crateItem )
+        {
+            ItemStack centerStack = grid.getStackInSlot(4);
+            if (centerStack != null && centerStack.getItem() == crateItem )
+            {
+                ItemStack containedStack = ItemBlockCrate.getContainingItemStack(centerStack);
+                if (centerStack != null)
+                {
+                    ItemBlockCrate.setContainingItemStack(result, containedStack);
+                }
+            }
+        }
+        return result;
+    }
+}
