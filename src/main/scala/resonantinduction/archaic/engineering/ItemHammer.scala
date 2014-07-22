c734a80f5dbd3b3e7348afedf9fa73c71fb9aaaa
Robert S <rseifert.phone@gmail.com>
2014-07-22 02:32:26 -0400

    Converted ItemHammer to scala

---
+++ src/main/scala/resonantinduction/archaic/engineering/ItemHammer.scala
@@ -0,0 +1,9 @@
+package resonantinduction.archaic.engineering;
+
+import net.minecraft.item.Item;
+
+/** Item used to interact with engineering table to crush ore */
+class ItemHammer extends Item {
+    setMaxStackSize( 1 );
+    setMaxDamage( 400 );
+}
