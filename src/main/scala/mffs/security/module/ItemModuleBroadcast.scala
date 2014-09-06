4a38409d078dc0b27e6eef17e319782e0817e662
Calclavia <admin@calclavia.com>
2014-09-06 22:38:44 +0800

    Fixed #MFFS-71 Server broadcast module crash

--- src/main/scala/mffs/security/module/ItemModuleBroadcast.scala
+++ src/main/scala/mffs/security/module/ItemModuleBroadcast.scala
@@ -6,7 +6,6 @@ import mffs.field.TileElectromagneticProjector
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.util.ChatComponentText
 import resonant.api.mffs.machine.IProjector
-import resonant.lib.utility.LanguageUtility
 import universalelectricity.core.transform.vector.Vector3
 
 class ItemModuleBroadcast extends ItemModuleDefense
@@ -16,12 +15,12 @@ class ItemModuleBroadcast extends ItemModuleDefense
     val proj = projector.asInstanceOf[TileElectromagneticProjector]
     val entities = getEntitiesInField(projector)
 
-    //TODO: CUSTOM BROADCAST MESSAGE!
+    //TODO: Add custom broadcast messages
     entities.view
       .filter(_.isInstanceOf[EntityPlayer])
       .map(_.asInstanceOf[EntityPlayer])
-      .foreach(_.addChatMessage(new ChatComponentText("[" + proj.getInventoryName + "] " + LanguageUtility.getLocal("message.moduleWarn.warn"))))
-
+      .foreach(_.addChatMessage(new ChatComponentText("[" + proj.getInventoryName + "] " + "Leave this zone immediately. You have no right to enter.")))
+    //LanguageUtility.getLocal("message.moduleWarn.warn")
     return false
   }
 }
