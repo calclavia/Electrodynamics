8f11e8fcf19b52290482b0e3b40bbd7e64eaa452
Calclavia <admin@calclavia.com>
2014-09-11 18:48:57 +0800

    Removed archives

--- archive/java/resonantinduction/mechanical/fluid/pipes/IPipeType.java
+++
@@ -1,8 +0,0 @@
-package resonantinduction.mechanical.fluid.pipes;
-
-import dark.lib.helpers.ColorCode.IColoredId;
-
-public interface IPipeType extends IColoredId
-{
-	public String getName(int pipeID);
-}
