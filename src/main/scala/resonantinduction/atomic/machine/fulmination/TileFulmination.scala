888bd053455c5ba0f850c3a16e3f2e9b93e2b72f
Robert S <rseifert.phone@gmail.com>
2014-09-29 15:10:58 -0400

    Converted Fulmination generator to scala

---
+++ src/main/scala/resonantinduction/atomic/machine/fulmination/TileFulmination.scala
@@ -0,0 +1,38 @@
+package resonantinduction.atomic.machine.fulmination
+
+import net.minecraft.block.material.Material
+import resonant.lib.content.prefab.java.TileElectric
+
+/**
+ * Fulmination TileEntity
+ */
+object TileFulmination
+{
+    private final val DIAN: Long = 10000000000000L
+}
+
+class TileFulmination extends TileElectric(Material.iron)
+{
+    //Constructor
+    energy.setCapacity(TileFulmination.DIAN * 2)
+    this.blockHardness(10)
+    this.blockResistance(25000)
+
+    override def start
+    {
+        super.start
+        FulminationHandler.INSTANCE.register(this)
+    }
+
+    override def update
+    {
+        super.update
+        energy.extractEnergy(10, true)
+    }
+
+    override def invalidate
+    {
+        FulminationHandler.INSTANCE.unregister(this)
+        super.start
+    }
+}
