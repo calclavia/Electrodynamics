a4f338c455633bf7ee601eda4eccc7026015e436
Calclavia <admin@calclavia.com>
2015-02-25 14:44:45 +0800

    Players now implement Damageable

--- src/main/scala/mffs/field/module/ItemModuleShock.scala
+++ src/main/scala/mffs/field/module/ItemModuleShock.scala
@@ -4,8 +4,9 @@ import mffs.base.ItemModule
 import mffs.field.BlockForceField
 import mffs.security.MFFSPermissions
 import nova.core.block.Block
-import nova.core.entity.Damage.DamageType
-import nova.core.entity.{Damage, Entity}
+import nova.core.entity.Entity
+import nova.core.entity.components.Damageable
+import nova.core.entity.components.Damageable.DamageType
 import nova.core.player.Player
 
 class ItemModuleShock extends ItemModule {
@@ -13,7 +14,7 @@ class ItemModuleShock extends ItemModule {
 	override def getID: String = "moduleShock"
 
 	override def onFieldCollide(block: Block, entity: Entity) {
-		if (entity.isInstanceOf[Damage]) {
+		if (entity.isInstanceOf[Damageable]) {
 			if (entity.isInstanceOf[Player]) {
 				val entityPlayer = entity.asInstanceOf[Entity with Player]
 				if (block.asInstanceOf[BlockForceField].getProjector.hasPermission(entityPlayer.getUsername, MFFSPermissions.forceFieldWarp)) {
@@ -21,7 +22,7 @@ class ItemModuleShock extends ItemModule {
 				}
 			}
 
-			entity.asInstanceOf[Damage].damage(count(), DamageType.generic)
+			entity.asInstanceOf[Damageable].damage(count(), DamageType.generic)
 		}
 
 		return true
