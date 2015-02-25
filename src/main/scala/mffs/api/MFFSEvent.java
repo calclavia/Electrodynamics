96f5f00ce1d278e9efca3b0d8fa611a760c593ef
Calclavia <admin@calclavia.com>
2015-02-25 22:18:53 +0800

    Merged ForceMobilize events to MFFSEvent

--- src/main/scala/mffs/api/MFFSEvent.java
+++ src/main/scala/mffs/api/MFFSEvent.java
@@ -14,6 +14,10 @@ public class MFFSEvent {
 
 	public EventBus<EventStabilize> stabilizeEventBus = new EventBus<>();
 
+	public EventBus<EventForceMobilize> checkMobilize = new EventBus<>();
+	public EventBus<EventForceMobilize> preMobilize = new EventBus<>();
+	public EventBus<EventForceMobilize> postMobilize = new EventBus<>();
+
 	public class EventStabilize {
 		public final Item item;
 		public final World world;
@@ -25,4 +29,17 @@ public class MFFSEvent {
 			this.pos = pos;
 		}
 	}
+
+	public abstract class EventForceMobilize {
+		public final World world;
+		public final Vector3i before;
+		public final Vector3i after;
+
+		public EventForceMobilize(World world, Vector3i before, Vector3i after) {
+			this.world = world;
+			this.before = before;
+			this.after = after;
+		}
+	}
+
 }
