3766a1e7a8c08525802923bf47069eb112e7f81e
Calclavia <admin@calclavia.com>
2015-05-09 09:57:47 +0800

    removed GridLayout unused constructor parameter

--- src/main/scala/mffs/gui/GuiFortronCapacitor.scala
+++ src/main/scala/mffs/gui/GuiFortronCapacitor.scala
@@ -1,16 +1,19 @@
 package mffs.gui
 
 import com.resonant.lib.wrapper.WrapFunctions._
+import com.resonant.wrapper.lib.utility.science.UnitDisplay
 import mffs.production.BlockFortronCapacitor
 import nova.core.game.Game
 import nova.core.gui.ComponentEvent.ActionEvent
 import nova.core.gui.components.inventory.Slot
 import nova.core.gui.components.{Button, Container, Label}
-import nova.core.gui.layout.{Anchor, FlowLayout}
+import nova.core.gui.layout.Constraints.GridLayoutConstraints
+import nova.core.gui.layout.{Anchor, FlowLayout, GridLayout}
 import nova.core.gui.{ComponentEvent, GuiEvent}
 import nova.core.network.NetworkTarget.Side
 
 import scala.collection.convert.wrapAll._
+
 /**
  * Fortron Capacitor Gui
  * @author Calclavia
@@ -44,7 +47,7 @@ class GuiFortronCapacitor extends GuiMFFS("fortronCapacitor") {
 		(0 to 2) foreach (i => upgrades.add(new Slot("main", i)))
 
 		//Input slots
-		val inputs = new Container("inputs").setLayout(new FlowLayout)
+		val inputs = new Container("inputs").setLayout(new GridLayout(classOf[GridLayoutConstraints]))
 		for (x <- 0 to 1; y <- 0 to 1)
 			inputs.add(new Slot("main", x + y))
 
@@ -58,15 +61,21 @@ class GuiFortronCapacitor extends GuiMFFS("fortronCapacitor") {
 		 */
 		add(
 			new Container().setLayout(new FlowLayout)
-				.add(new Label("linkedDevice", Game.instance.languageManager.translate("linkedDevice", Map("%1" -> (block.getDeviceCount + "")))))
-				//.add(new Label("transmissionRate", Game.instance.languageManager.translate("transmissionRate", Map("%1" -> (new UnitDisplay(UnitDisplay.Unit.LITER, block.getTransmissionRate * 20).symbol() + "/s")))))
+				.add(new Label("linkedDevice", Game.instance.languageManager.translate("gui.linkedDevice", Map("%1" -> (block.getDeviceCount + "")))))
+				.add(
+					new Label(
+						"transmissionRate",
+						Game.instance.languageManager.translate("gui.transmissionRate", Map("%1" -> (new UnitDisplay(UnitDisplay.Unit.LITER, block.getTransmissionRate * 20).symbol() + "/s")))
+					)
+				)
 				.add(upgrades)
 				.add(inputs)
 				.add(outputs)
 				//Toggle button
 				.add(
-					new
-							Button("toggle", "Toggle Mode").setPreferredSize(80, 20).onEvent((evt: ActionEvent, component: Button) => block.toggleTransferMode(), classOf[ComponentEvent.ActionEvent], Side.SERVER)
+					new Button("toggle", "Toggle Mode")
+						.setPreferredSize(80, 20)
+						.onEvent((evt: ActionEvent, component: Button) => block.toggleTransferMode(), classOf[ComponentEvent.ActionEvent], Side.SERVER)
 				)
 			, Anchor.CENTER
 		)
