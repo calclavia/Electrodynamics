646142060dd0017c950a5bf8ee0b73a64a8c77cc
Calclavia <admin@calclavia.com>
2015-05-27 16:29:19 +0800

    Wrote basic wire connection logic

--- electric/src/main/scala/com/calclavia/edx/electric/circuit/wire/flat/BlockWire.scala
+++ electric/src/main/scala/com/calclavia/edx/electric/circuit/wire/flat/BlockWire.scala
@@ -7,13 +7,14 @@ import com.calclavia.edx.core.component.Material
 import com.calclavia.edx.electrical.circuit.wire.base.WireMaterial
 import com.calclavia.graph.api.energy.NodeElectric
 import com.calclavia.graph.core.electric.NodeElectricJunction
-import com.calclavia.microblock.api.Microblock
+import com.calclavia.microblock.api.{Microblock, MicroblockContainer}
 import com.resonant.lib.util.RotationUtility
 import com.resonant.lib.wrapper.WrapFunctions._
 import nova.core.block.Block
 import nova.core.block.Block.BlockPlaceEvent
 import nova.core.block.component.{BlockCollider, StaticBlockRenderer}
 import nova.core.entity.Entity
+import nova.core.game.Game
 import nova.core.network.{PacketHandler, Sync}
 import nova.core.render.model.{BlockModelUtil, Model, StaticCubeTextureCoordinates}
 import nova.core.retention.{Storable, Stored}
@@ -118,25 +119,124 @@ class BlockWire extends Block with Storable with PacketHandler {
 		var newConnectionMask = 0x00000000
 		var connections = Set.empty[NodeElectric]
 
-		for (s <- 0 until 4) {
-			val absDir = RotationUtility.rotateSide(side, s)
-			//Check inner connection
-			if (get(classOf[Microblock]).)
+		for (relativeSide <- 0 until 4) {
+			val absSide = RotationUtility.rotateSide(relativeSide, relativeSide)
 
+			if (maskOpen(absSide)) {
+				if (!computeInnerConnection(relativeSide, absSide)) {
+					if (!computeStraightConnection(relativeSide, absSide)) {
+						computeStraightConnection(relativeSide, absSide)
+					}
+				}
+			}
+		}
 
-			//Check straight connection
-
-			//Check corner connection
+		//Apply connection masks
+		if (newConnectionMask != connectionMask) {
+			connectionMask = newConnectionMask
+			//Update client render
+			Game.instance.networkManager.sync(1, this)
 		}
 
 		return connections
+
+		/**
+		 * Check inner connection (01)
+		 * @return True if a connection is found
+		 */
+		def computeInnerConnection(relativeSide: Int, absSide: Int): Boolean = {
+			val opMicroblock = get(classOf[MicroblockContainer]).getMicroblock(absSide)
+			if (opMicroblock.isPresent) {
+				val otherMicroblock = opMicroblock.get()
+				val opElectric = otherMicroblock.block.getOp(classOf[NodeElectric])
+
+				if (opElectric.isPresent) {
+					connections += opElectric.get
+					newConnectionMask |= 0x01 << (relativeSide * 2)
+					return true
+				}
+			}
+			return false
+		}
+
+		/**
+		 * Check straight connection (11)
+			@return True if a connection is found
+		 */
+		def computeStraightConnection(relativeSide: Int, absSide: Int): Boolean = {
+			//The position to check for another wire or a device.
+			val checkPos = position + Direction.fromOrdinal(absSide).toVector
+			val checkBlock = world.getBlock(checkPos)
+
+			if (checkBlock.isPresent) {
+
+				//First check for microblocks for another wire
+				val opMicroblockHolder = checkBlock.get.getOp(classOf[MicroblockContainer])
+				if (opMicroblockHolder.isPresent) {
+					//Try to find the microblock that is has the component NodeElectric
+					val opMicroblock = opMicroblockHolder.get().getMicroblock(this.side)
+					if (opMicroblock.isPresent) {
+						val opElectric = opMicroblock.get.block.getOp(classOf[NodeElectric])
+
+						if (opElectric.isPresent) {
+							connections += opElectric.get
+							newConnectionMask |= 0x10 << (relativeSide * 2)
+							return true
+						}
+					}
+				}
+
+				//A microblock is not present. Try checking if the block is electric
+				val opElectric = checkBlock.get.getOp(classOf[NodeElectric])
+
+				if (opElectric.isPresent) {
+					connections += opElectric.get
+					newConnectionMask |= 0x10 << (relativeSide * 2)
+					return true
+				}
+			}
+			return false
+		}
+
+		/**
+		 * Check corner connection (11)
+		 * @return True if a connection is found
+		 */
+		def computeCornerConnection(relativeSide: Int, absSide: Int): Boolean = {
+			/**
+			 * The position to check for another wire or a device.
+			 * Our position check has to move one block towards the side, then one block towards the side the wire is attached to.
+			 */
+			val checkPos = position + Direction.fromOrdinal(absSide).toVector + Direction.fromOrdinal(side).toVector
+			val checkBlock = world.getBlock(checkPos)
+
+			if (checkBlock.isPresent) {
+				val opMicroblockHolder = checkBlock.get.getOp(classOf[MicroblockContainer])
+				if (opMicroblockHolder.isPresent) {
+					//Try to find the microblock that is has the component NodeElectric
+					//We look for opposite of the side we are checking, as the block has to be flat placed onto the same block this wire is flat-placed on.
+					val opMicroblock = opMicroblockHolder.get().getMicroblock(absSide ^ -1)
+					if (opMicroblock.isPresent) {
+						val opElectric = opMicroblock.get.block.getOp(classOf[NodeElectric])
+
+						if (opElectric.isPresent) {
+							connections += opElectric.get
+							newConnectionMask |= 0x11 << (relativeSide * 2)
+							return true
+						}
+					}
+				}
+			}
+			return false
+		}
+
+		/**
+		 * Check if there's a cover on a specific side
+		 */
+		def maskOpen(absSide: Int): Boolean = {
+			return get(classOf[MicroblockContainer]).getMicroblock(absSide).isPresent
+		}
 	}
 
-	/**
-	 * Check if there's a cover on a specific side
-	 */
-	def maskOpen(absDir: Int): Boolean = {
-		//TODO: Implement me
-		return false
-	}
+	override def getID: String = "electricWire"
 }
