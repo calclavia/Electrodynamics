3c51e54da9923f15164608378cafaa0f4e383bd1
Calclavia <admin@calclavia.com>
2015-06-15 10:35:26 +0800

    Fixed compiler errors from NOVA

--- optics/src/main/scala/com/calclavia/edx/optics/content/OpticsModels.scala
+++ optics/src/main/scala/com/calclavia/edx/optics/content/OpticsModels.scala
@@ -1,7 +1,7 @@
 package com.calclavia.edx.optics.content
 
 import com.calclavia.edx.core.Reference
-import nova.core.render.model.TechneModel
+import nova.core.render.model.TechneModelProvider
 import nova.scala.modcontent.ContentLoader
 
 /**
@@ -10,16 +10,16 @@ import nova.scala.modcontent.ContentLoader
  */
 object OpticsModels extends ContentLoader {
 
-	val fortronCapacitor = new TechneModel(Reference.domain, "fortronCapacitor")
-	val projector = new TechneModel(Reference.domain, "electromagneticProjector")
-	val biometric = new TechneModel(Reference.domain, "biometricIdentifier")
-	val mobilizer = new TechneModel(Reference.domain, "forceMobilizer")
-	val deriver = new TechneModel(Reference.domain, "coercionDeriver")
+	val fortronCapacitor = new TechneModelProvider(Reference.domain, "fortronCapacitor")
+	val projector = new TechneModelProvider(Reference.domain, "electromagneticProjector")
+	val biometric = new TechneModelProvider(Reference.domain, "biometricIdentifier")
+	val mobilizer = new TechneModelProvider(Reference.domain, "forceMobilizer")
+	val deriver = new TechneModelProvider(Reference.domain, "coercionDeriver")
 
-	val laserReceiver = new TechneModel(Reference.domain, "laserReceiver")
-	val laserEmitter = new TechneModel(Reference.domain, "laserEmitter")
-	val mirror = new TechneModel(Reference.domain, "mirror")
-	val crystal = new TechneModel(Reference.domain, "crystal")
+	val laserReceiver = new TechneModelProvider(Reference.domain, "laserReceiver")
+	val laserEmitter = new TechneModelProvider(Reference.domain, "laserEmitter")
+	val mirror = new TechneModelProvider(Reference.domain, "mirror")
+	val crystal = new TechneModelProvider(Reference.domain, "crystal")
 
 	override def id: String = Reference.id
 }
