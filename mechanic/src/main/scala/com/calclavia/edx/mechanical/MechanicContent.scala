d2a7df03417ab17f235a7458cc0ab90b689c05a3
Calclavia <admin@calclavia.com>
2015-07-28 14:25:38 -0400

    Added gears obj file

--- mechanic/src/main/scala/com/calclavia/edx/mechanical/MechanicContent.scala
+++ mechanic/src/main/scala/com/calclavia/edx/mechanical/MechanicContent.scala
@@ -4,14 +4,20 @@ import com.calclavia.edx.core.Reference
 import com.calclavia.edx.mechanical.mech.gear.BlockGear
 import com.calclavia.edx.mechanical.mech.gearshaft.BlockGearshaft
 import nova.core.block.BlockFactory
+import nova.core.render.model.WavefrontObjectModelProvider
 import nova.core.render.texture.BlockTexture
 import nova.scala.modcontent.ContentLoader
 
-
-object MechanicContent extends ContentLoader{
+object MechanicContent extends ContentLoader {
 	override def id: String = Reference.mechanicID
+
+	//Textures
 	val gearTexture = new BlockTexture(Reference.domain, "wire")
 	lazy val gearshaftTexture = gearTexture
+
+	//Models
+	val modelGear = new WavefrontObjectModelProvider(Reference.domain, "gears")
+
 	//Blocks
 	var blockGear: BlockFactory = classOf[BlockGear]
 	var blockGearshaft: BlockFactory = classOf[BlockGearshaft]
