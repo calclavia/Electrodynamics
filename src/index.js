35bef0a4645c08d401467a183c638b712360afd5
Calclavia <admin@calclavia.com>
2015-12-18 20:58:30 -0800

    Updated LTM API

--- src/index.js
+++ src/index.js
@@ -15,20 +15,7 @@ function main() {
     'Test Block',
     'RIGHT_CLICK_BLOCK',
     function event_handler(event) {
-      //Test function syntax sugar:
-      event.world.addEntity = event.world.spawnEntityInWorld;
-      /**
-       * Spawn an entity based on an entity prefab.
-       * Hide this!
-       */
-      spawnEntity = function(prefab) {
-        var Entity = Java.type("com.learntomod.entity.wrapper.Entity");
-        var entity = new Entity(event.world);
-        prefab.pipe(entity);
-        event.world.spawnEntityInWorld(entity);
-        return entity;
-      };
-      var ent = spawnEntity(EntityManager.registry().get("turbine"));
+      var ent = EntityManager.instantiate(event.world, "turbine");
       ent.setPosition(event.pos.getX() + 0.5, event.pos.getY() + 1, event.pos.getZ() + 0.5);
     }
   );
@@ -36,15 +23,23 @@ function main() {
   EntityManager.create("turbine")
     .then(function(entity) {
       entity.setSize(1, 1);
-      entity.update = function() {
-        entity.components().put("frame", (entity.components().get("frame") + 2) % 60);
-      }
 
-      //Set Renderer
-      entity.renderer
-        .then(function(renderer) {
-          turbineModel.setFrame(entity.components().get("frame"));
-          renderer.addChild(turbineModel);
+      //Update event
+      entity
+        .on("Update")
+        .bind(function() {
+          entity.components().put("frame", (entity.components().get("frame") + 2) % 60);
+        });
+
+      //Renderer event
+      entity
+        .on("Render")
+        .bind(function(evt) {
+          evt.pipeline
+            .then(function(renderer) {
+              turbineModel.setFrame(entity.components().get("frame"));
+              renderer.addChild(turbineModel);
+            });
         });
       return entity;
     });
